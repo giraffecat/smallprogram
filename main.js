7bc2758848c650a2f05391291d0d56df382f1527
hui <1018997102@qq.com>
2017-07-21 15:16:08 +0800

    m

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-
+var arr=[2,4,5,9,1];
+var arr2=arr.sort();
+console.log(arr2)
--- main2.js
+++ main2.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 const merge=require('./merge.js');
 var collectionA=[1,2,5,4,7,555,77];
 var collectionB=[4,7,8,9,6,6];
 var result=merge.mergearr(collectionA,collectionB);
 console.log(result);
-=======
 
->>>>>>> cc9311c615c344bf4aa2f5b3b6ebfc4d0b295bad
